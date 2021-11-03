881c090bcd36d1035cb3e77ed9d2f7aa8737ff34
Teodora Hoamea <teodora.hoamea@gmail.com>
2021-11-03 12:08:04 +0200

    Added BuilderReport

---
+++ RenewableEnergyCalculator/Utils/Reports/BuilderReport/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builderReport
+{
+    public class Data
+    {
+        public GeographicalPoint geographicalPoint { get; set; }
+        public PanelType panelType{ get; set; }
+        public ResultEnergyData resultEnergyData { get; set; }
+        public PowerCurve powerCurve { get; set; }
+
+    }
+}
---
+++ RenewableEnergyCalculator/Utils/Reports/BuilderReport/IDataReportBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace builderReport
+{
+    public interface IDataReportBuilder
+    {
+        void BuildHeader();
+        void BuildBody();
+        void BuildFooter();
+        DataReport GetReport();
+    }
+}
