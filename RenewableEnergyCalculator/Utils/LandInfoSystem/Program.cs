f113c35af0b859928399dbfe5fe755de0fe9c60e
Barbu-Alexandru <alecsx344@gmail.com>
2021-11-03 15:10:51 +0200

    Added LandInfoSystem

---
+++ RenewableEnergyCalculator/Utils/LandInfoSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LandInfoSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
