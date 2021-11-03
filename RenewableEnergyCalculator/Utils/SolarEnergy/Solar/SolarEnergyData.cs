3c6c8a84b4717ccddc38cba4856bf53364ae5eea
Raluca Bucnaru(Rossellia) <raluca.bucnaru@outlook.com>
2021-11-03 11:32:05 +0200

    Added Raluca's part (Solar Energy)

---
+++ RenewableEnergyCalculator/Utils/SolarEnergy/Solar/SolarEnergyData.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace REIC
+{
+
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FileName: SolarEnergyData.cs
+    //FileType: Visual C# Source file
+    //Author : Bucnaru Raluca
+    //Description : yearly energy generation for one panel; here we have all the variables we need for the equation
+    //////////////////////////////////////////////////////////////////////////////////////
+    class SolarEnergyData
+    {
+        //same names as in the equation from Irina's part
+        float A; //area
+        float r; //efficiency
+        float H; //solar radiation
+        float PR; //performance rate
+
+        void setArea(SolarPanel panel) { A = panel.getArea(); }
+        void setEfficiency(SolarPanel panel) { r = panel.getEff(); }
+
+        void CalculatePR(PerformanceRatio performance) { }
+
+        void SetRadiation(SolarRadiation radiation) { H = radiation.getAverageRadiation(); }
+
+
+        public float getA() { return A; }
+        public float getr() { return r; }
+        public float getH() { return H; }
+        public float getPR() { return PR; }
+
+        public float CalculateEnergy() { float E = A * r * H * PR; return E; }
+    }
+  
+}
---
+++ RenewableEnergyCalculator/Utils/SolarEnergy/Solar/SolarRadiation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+namespace REIC
+{
+    /// ////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //FileName: SolarRadiation.cs
+    //FileType: Visual C# Source file
+    //Author : Bucnaru Raluca
+    //Description : get the solar radiation from the nasa api; we'll apply specific data from all those variable lists depending on how and where our panel is placed
+    //////////////////////////////////////////////////////////////////////////////////////
+
+    class SolarRadiation
+    {
+        //using the same names as those from the api
+        List<float> SI_EF_TILTED_SURFACE_HORIZONTAL;
+        List<float> SI_EF_TILTED_SURFACE_LAT_MINUS15;
+        List<float> SI_EF_TILTED_SURFACE_LATITUDE;
+        List<float> SI_EF_TILTED_SURFACE_LAT_PLUS15;
+        List<float> SI_EF_TILTED_SURFACE_VERTICAL;
+        List<float> SI_EF_TILTED_SURFACE_OPTIMAL;
+        List<float> SI_EF_TILTED_SURFACE_OPTIMAL_ANG;
+        string SI_EF_TILTED_SURFACE_OPTIMAL_ANG_ORT;
+
+        float annualSumOfRadiation;
+
+        //we get the average radiation of each month, so we just have to sum them all up to get the yearly radiation
+        void annualRadiation() { foreach (var it in SI_EF_TILTED_SURFACE_HORIZONTAL) { annualSumOfRadiation += it; } } //this is just an example, we can probably use all those variables depending on the case
+        public float getAverageRadiation() { return annualSumOfRadiation; }
+        public string getOptimalAngOrt() { return SI_EF_TILTED_SURFACE_OPTIMAL_ANG_ORT; }
+
+    }
+}
